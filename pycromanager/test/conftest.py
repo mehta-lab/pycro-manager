e174770f1a6ad6f7c33d0c032e24e4cd7e6c4b7c
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2022-11-23 14:41:12 -0800

    enable zmq before testing

---
+++ pycromanager/test/conftest.py
@@ -0,0 +1,17 @@
+from pytest import fixture
+import os
+import glob
+import json
+
+
+@fixture(scope="session")
+def enable_zmq():
+    # FIXME: this enviroment variable only works on Windows
+    profile_path = os.path.expandvars(r"%LOCALAPPDATA%\Micro-Manager\UserProfiles")
+    profile_file = glob.glob(os.path.join(profile_path, "Default_*.json"))
+    with open(profile_file, "+") as f:
+        profile = json.load(f)
+        profile["map"]["Preferences"]["scalar"]["org.micromanager.internal.MMStudio"][
+            "scalar"
+        ]["run ZMQ server"]["scalar"] = True
+        json.dump(profile, f)
--- pycromanager/test/test_acq_util.py
+++ pycromanager/test/test_acq_util.py
@@ -1,8 +1,11 @@
 import os
+
 # tested code
 from pycromanager import start_headless
 
-def test_start_headless():
+
+def test_start_headless(enable_zmq):
+    _ = enable_zmq
     mm_app_path = "C:\Program Files\Micro-Manager-2.0"
     config_file = os.path.join(mm_app_path, "MMConfig_demo.cfg")
     start_headless(mm_app_path, config_file, timeout=5000)
